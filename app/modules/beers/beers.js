96ac513bbd5c2c140d65e378f7662d29d262589f
Suissa <jnascimento@gmail.com>
2015-03-12 23:41:40 -0300

    Listando as cervejas

--- app/modules/beers/beers.js
+++ app/modules/beers/beers.js
@@ -14,6 +14,29 @@
 
   function BeersCtrl($scope) {
     $scope.msg = 'Listagem das cervejas';
+    $scope.reverse = false;
+    $scope.predicate = 'name';
+
+    $scope.ordenar = function(predicate) {
+      $scope.predicate = predicate;
+      $scope.reverse = !$scope.reverse
+    }
+  // criamos um array de cervejas
+    var cervejas = [{
+      name: 'Kaiser', price: 2
+      }, {
+        name: 'Skol', price: 3
+      }, {
+        name: 'Glacial', price: 4
+      }, {
+        name: 'Polar', price: 6
+      }, {
+        name: 'Heineken', price: 10
+      }
+    ];
+  // instanciamos nosso array no nosso scope
+  // para que tenhamos acesso à esse array na View
+    $scope.cervejas = cervejas;
   }
 
   BeersCtrl.$inject = ['$scope'];
